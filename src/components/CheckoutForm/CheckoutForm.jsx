5eaa9b7ab6f810ce212f5872e94867d3b6228437
Gonzor <gonzalo.marodriguez@gmail.com>
2023-05-22 00:28:42 -0300

    add confirmation e-mail

--- src/components/CheckoutForm/CheckoutForm.jsx
+++ src/components/CheckoutForm/CheckoutForm.jsx
@@ -2,13 +2,19 @@ import { useState } from 'react';
 import './CheckoutForm.css';
 
 export const CheckoutForm = ({ onConfirm }) => {
-
     const [name, setName] = useState('');
     const [phone, setPhone] = useState('');
     const [email, setEmail] = useState('');
+    const [confirmEmail, setConfirmEmail] = useState('');
+    const [error, setError] = useState('');
 
     const handleConfirm = (event) => {
         event.preventDefault();
+
+        if (email !== confirmEmail) {
+            setError('Error en los correos, por favor verifica ambos campos.');
+            return;
+        }
 
         const userData = {
             name,
@@ -22,8 +28,9 @@ export const CheckoutForm = ({ onConfirm }) => {
         <div className='checkoutForm'>
             <form onSubmit={handleConfirm} className='form'>
                 <label className='label'>
-                    Nombre:
-                    <input className='input' 
+                    Nombre y Apellido:
+                    <input
+                        className='input' 
                         type="text"
                         value={name}
                         onChange={({target}) => setName(target.value)}
@@ -32,7 +39,8 @@ export const CheckoutForm = ({ onConfirm }) => {
                 </label>
                 <label className='label'>
                     Telefono:
-                    <input className='input' 
+                    <input
+                        className='input' 
                         type="text"
                         value={phone}
                         onChange={({target}) => setPhone(target.value)}
@@ -41,17 +49,29 @@ export const CheckoutForm = ({ onConfirm }) => {
                 </label>
                 <label className='label'>
                     Email:
-                    <input className='input' 
+                    <input
+                        className='input' 
                         type="email"
                         value={email}
                         onChange={({target}) => setEmail(target.value)}
                         required
                     />
                 </label>
+                <label className='label'>
+                    Confirmar Email:
+                    <input
+                        className='input' 
+                        type="email"
+                        value={confirmEmail}
+                        onChange={({target}) => setConfirmEmail(target.value)}
+                        required
+                    />
+                </label>
+                {error && <p className='error'>{error}</p>}
                 <div className='label'>
                     <button type='submit' className='button'>Generar Orden</button>
                 </div>
             </form>
         </div>
-    )
-}
+    );
+};
