275aacc1fe1cca3344d9e2bdd936496acb9364d0
Gonzor <gonzalo.marodriguez@gmail.com>
2023-05-21 22:59:03 -0300

    cambios finales

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+console.error = (message) => {
+  if (typeof message === 'string' && message.includes('validateDOMNesting')) {
+    return;
+  }
+};
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
